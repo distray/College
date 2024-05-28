747458863c08482abaacf0d3d8242dd653325bbe
jellypack <52904880+distray@users.noreply.github.com>
2024-05-28 09:03:02 -0500

    Update mafpercents.java
    Ch2 1st Assignment

--- C1/mafpercents.java
+++ C1/mafpercents.java
@@ -1,1 +1,23 @@
+package Ch2A;
+import java.util.Scanner;
 
+public class mafpercents {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            System.out.print("How many males are registered in the class? "); // males input #
+            int numMales = scanner.nextInt();
+            System.out.print("How many females are registered in the class? "); // females input #
+            int numFemales = scanner.nextInt(); 
+
+            int totalNum = numMales + numFemales; // percentage calc
+            double percentMale = ((double) numMales / totalNum) * 100;
+            double percentFemale = ((double) numFemales / totalNum) * 100;
+
+            // display final percents--experimented with using variables first and using % as text after calculations to optimize
+            System.out.println((percentMale) + "% Males");
+            System.out.println((percentFemale) + "% Females");
+
+            // fix resource leak from problems terminal
+            scanner.close();
+    }
+}
